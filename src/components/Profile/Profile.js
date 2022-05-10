f286d051c54be1509a1bb2dca4da2f52231d6c9b
LDanchenko <2l-u-b-o-v@ukr.net>
2022-05-11 00:50:00 +0300

    Task 1 done

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import style from './Profile.module.css';
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={style.profile}>
+      <div className={style.description}>
+        <img src={avatar} alt={username} className={style.avatar} />
+        <p className={style.username}>{username}</p>
+        <p className={style.tag}>@{tag}</p>
+        <p className={style.location}>{location}</p>
+      </div>
+      <ul className={style.stats}>
+        <li>
+          <span className={style.label}>Followers</span>
+          <span className={style.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={style.label}>Views</span>
+          <span className={style.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={style.label}>Likes</span>
+          <span className={style.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Profile;
